b60fc3cdefc736bf20971b09ae75f366ddf23843
Barcelos <barcelosps41710@gmail.com>
2023-08-26 14:59:19 -0300

    exercise 1 completo

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
 import './styles/App.css';
+import { Routes, Route } from 'react-router-dom';
 import { About, NotFound, Posts, Users } from './pages';
 
 function App() {
-  return (<p>Exercício Bloggers - React Router</p>);
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={ <Users /> } />
+        <Route path="/posts/:id" element={ <Posts /> } />
+        <Route path="/about" element={ <About /> } />
+        <Route path="*" element={ <NotFound /> } />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
