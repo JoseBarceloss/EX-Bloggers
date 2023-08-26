5cacb1c527b17ab4b8b0f2142c113074e64968d7
Barcelos <barcelosps41710@gmail.com>
2023-08-26 15:33:33 -0300

    eu quis colocar no lugar de post o nome de cada um, dei uma comentada no codigo para eu entender melhor oque cada coisa faz.

--- src/pages/Posts.tsx
+++ src/pages/Posts.tsx
@@ -1,8 +1,8 @@
 import { useParams } from 'react-router-dom';
 import Header from '../components/Header';
 import Post from '../components/Post';
-import { posts } from '../data';
-import { PostType } from '../types';
+import { posts, users } from '../data';
+import { PostType, UserType } from '../types';
 import '../styles/Posts.css';
 
 export default function Posts() {
@@ -12,10 +12,15 @@ export default function Posts() {
   // Filtra os posts com base no ID do usuário extraído dos parâmetros de rota
   const userPosts: PostType[] = posts.filter((post) => post.userId === Number(id));
 
+  // Encontra o nome do usuário a partir do ID e define um título padrão caso não haja correspondência
+  const foundUser = users.find((user: UserType) => user.id === Number(id));
+  const userTitle = foundUser ? `Posts by ${foundUser.firstName} ${foundUser.lastName} `
+    : 'No Posts for this User';
+
   return (
     <div data-testid="posts-page">
       <Header />
-      <h1>Posts</h1>
+      <h1>{userTitle}</h1>
       <div className="posts-list">
         {userPosts.map((post) => (
           <Post key={ post.id } postData={ post } />
